453a09fb263c87547ef50e25c77b3a80f072eee2
Leo9723 <pala9761@gmail.com>
2023-01-10 14:47:12 +0100

    Caricamento parziale esercizio

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            Title: 'La mia prima app con vite!'
+        }
+    }
+}
+</script>
+<template>
+        {{ Title }}
+</template>
+<style>
+    
+</style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "Jumbotron",
+    data() {
+        return {
+            JumboImg: '../assets/jumbo.jpg'
+        }
+    },
+}
+</script>
+<template>
+        {{ JumboImg }}
+</template>
+<style>
+    
+</style>
